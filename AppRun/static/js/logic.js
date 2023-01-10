4b1160a1e268f5a5afec2eaa566fc0a468ea78ff
elina dart <dartelina4@gmail.com>
2023-01-10 12:58:49 -0500

    checking all run without errors

--- AppRun/static/js/logic.js
+++ AppRun/static/js/logic.js
@@ -1,103 +1,51 @@
-function init() {
-    // Grab a reference to the dropdown select element
-    var selector = d3.select("#selDataset");
+function saveDataToPickle() {
+
+  // Create an empty list
   
-    // Use the list of sample names to populate the select options
-    d3.json("static/js/dataframe.json").then((data) => {
-    //   var colNames = data.General_Health_Status;
-        
-      console.log("working...");
-    //   colNames.forEach((value) => {
-    //     selector
-    //       .append("option")
-    //       .text(value)
-    //       .property("value");
-    //   });
+  var data = [];
 
-      data = [
-        { name: "Diabetes", category: ["yes", "no"]},
-        { name: "Prediabetes", category: ["yes", "no"]},
-        { name: "Weight_Lbs", category: []},
-        { name: "Categorical_BMI", category: ["Obese", 
-                                            "Overweight", 
-                                            "Healthy_Weight", 
-                                            "Underweight"]},
-        { name: "Age", category: [] },
-        { name: "Gender", category: ["Male", "Female"]},
-        { name: "Race", category: ['White_Only',
-                                    'African_American_Only',
-                                    'Asian_Only',
-                                    'AIAN_Only',
-                                    'AIAN_AND_other',
-                                    'Other']},
-        { name: 'Education', category: ['Grade_1-11', 
-                                        '12th_Grade_no_diploma', 
-                                        'GED_Equivalent', 
-                                        'High_School_Graduate', 
-                                        'Some_College_no_degree',
-                                        'Associates_Occupational_Technical_Vocational',
-                                        'Associates_Academic_Program',
-                                        'Bachelor',
-                                        'Masters',
-                                        'Greater_Than_Master']},
-        { name: "Poverty_Ratio", category: ""}
-      ];
-    
-      // Get the unique values in the "category" column
-      let categories = [...new Set(data.map(item => item.category))];
-    
-      // Create buttons for each unique value
-      let buttons = categories.map(category => {
-        let button = document.createElement("button");
-        button.innerText = category;
-        button.addEventListener("click", () => filterData(category));
-        return button;
-      });
-    
-      // Add the buttons to the page
-      let selectors = document.getElementById("selDataset");
-      buttons.forEach(button => selectors.appendChild(button));
-  
-      // Use the first sample from the list to build the initial plots
-    //   var firstSample = sampleNames[0];
-    //   buildCharts(firstSample);
-    //   buildMetadata(firstSample);
-    });
-  }
-  
-  // Initialize the dashboard
-  init();
+  // Retrieve the data from the input fields
 
+  data.push(document.querySelector('#prediabetes').value);
+  data.push(document.querySelector('#weightlbs').value);
+  data.push(document.querySelector('#height').value);
+  data.push(document.querySelector('#age').value);
+  data.push(document.querySelector('#gender').value);
+  data.push(document.querySelector('#education').value);
+  data.push(document.querySelector('#race').value);
+  data.push(document.querySelector('#hhincome').value);
+  data.push(document.querySelector('#hhsize').value);
 
-// function that converts input to poverty ratio for ML
-function povertyRatio(householdIncome, numInHh) {
-    let povertyRatio = householdIncome / 8340 + (numInHh * 4540);
-    return povertyRatio;
-  }
-  
-
-try{
-let householdIncome = parseFloat(prompt("Enter the total income: "));
-let numInHh = parseFloat(prompt("Enter the number of people in the household: "));
-
-let povertyRatio = povertyRatio(householdIncome, numInHh);
-console.log(`The poverty ratio is ${povertyRatio}`);
-} catch(error) {    
-console.log(error.message);
+  return data;
 }
 
+function handleClick() {
+  let submitButton = document.querySelector("#submit-button");
+  submitButton.addEventListener('click', function(event) {
 
-// function that converts age input to integer for ML
-function toInt(input) {
-    return parseInt(input, 10);
-  }
+      // Collect form data and store it in a list
 
-// age conversion
-let age = prompt("Enter age: ");
-let ageValue = toInt(age);
-console.log(`The integer value is: ${ageValue}`);
+      event.preventDefault();
+      var data = saveDataToPickle();
 
-// weight conversion
-let weight = prompt("Enter weight in pounds: ")
-let weightValue = toInt(weight);
-console.log(`The integer value is: ${weightValue}`);
+      // Define the object with predetermined keys
+
+      var dataObj = {
+          Prediabetes: data[0],
+          Weight_Lbs: data[1],
+          height: data[2],
+          Age: data[3],
+          Gender: data[4],
+          Education: data[5],
+          Race: data[6],
+          HHincome: data[7],
+          HHcount: data[8]
+      };
+
+      // Log the object to the console
+
+      console.log(dataObj);
+  });
+};
+
+handleClick();
